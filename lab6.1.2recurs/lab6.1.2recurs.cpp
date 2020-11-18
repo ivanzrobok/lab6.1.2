f601c16f50bd665f17feaed1786609e0b14a37d9
ivanzrobok <ivan.zrobok.itis.2020@lpnu.ua>
2020-11-18 13:20:17 +0200

    lab6

---
+++ lab6.1.2recurs/lab6.1.2recurs.cpp
@@ -0,0 +1,51 @@
+﻿// lab6.1.2recurs.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+using namespace std;
+void create(int* arr, const int n, int i)
+{
+    arr[i] = rand() % 50;
+    if (i < n - 1)
+        create(arr, n, i + 1);
+}
+void print(int* arr, const int n,int i) {
+    cout <<  arr[i]<< " ";
+    if (i < n - 1)
+        print(arr, n, i + 1);
+    else
+        cout << endl;
+}
+int sum(int* arr, int const n, int s, int i) {
+
+    if (arr[i] > 0 && arr[i] % 2 == 1)
+        s += arr[i];
+    if (i < n - 1)
+        return sum(arr, n, s, i + 1);
+    else
+        return s;
+
+
+}
+void count(int* arr, const int n, int s, int i)//згідно варіанту функція виводить всі додатні і непарні елементи масиву.
+{
+    if (arr[i] > 0 && arr[i] % 2 == 1)
+        cout << arr[i] << " ";
+    if (i < n - 1)
+        count(arr, n, s, i + 1);
+    else
+        cout << endl;
+
+}
+
+
+int main()
+{
+    int const n = 23;
+    int p[n];
+    create(p, n, 0);
+    print(p, n,0);
+    cout << "suma modefication array " << sum(p, n,0,0) << endl;
+    count(p, n,0,0);
+    cout << endl;
+    
+}
---
+++ lab6.1.2test/lab6.1.2test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab612test
+{
+	TEST_CLASS(lab612test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
